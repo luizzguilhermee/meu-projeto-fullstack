22f21c8df543642d45e8905aa2875e1ad99eef85
Luiz Guilherme <luizanaeana@gmail.com>
2025-09-16 19:56:26 -0300

    Projeto Básico Fullstack

---
+++ frontend/cadastro-de-usuarios/devclub-cadastro-usuarios/src/pages/Home/index.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState, useRef } from "react";
+import "./style.css";
+import Trash from "../../assets/Trash.svg";
+import api from "../../services/api";
+
+//React Hooks
+
+function Home() {
+  const [users, setUsers] = useState([]);
+
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
+
+  async function getUsers() {
+    const usersFromApi = await api.get("/usuarios");
+
+    setUsers(usersFromApi.data);
+  }
+
+  async function createUsers() {
+    await api.post("/usuarios", {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value,
+    });
+    getUsers();
+  }
+
+  async function deleteUsers(id){
+    await api.delete(`/usuarios/${id}`)
+
+    getUsers();
+
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <div className="container">
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input
+          placeholder="Digite seu nome completo"
+          name="nome"
+          type="text"
+          ref={inputName}
+        />
+        <input
+          placeholder="Digite sua idade"
+          name="idade"
+          type="number"
+          ref={inputAge}
+        />
+        <input
+          placeholder="Digite seu e-mail"
+          name="email"
+          type="email"
+          ref={inputEmail}
+        />
+        <button type="button" onClick={createUsers}>
+          {" "}
+          Cadastrar{" "}
+        </button>
+      </form>
+
+      {users.map((user) => (
+        <div key={user.id} className="card">
+          <div>
+            <p>Nome: {user.name} </p>
+
+            <p>Idade: {user.age} </p>
+
+            <p>Email: {user.email} </p>
+          </div>
+
+          <button onClick={() => deleteUsers(user.id)}>
+            <img src={Trash} />
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
